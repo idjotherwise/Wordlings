640a62e1e91c3863d42ace7a07717a2a275e9358
Documenter.jl <documenter@juliadocs.github.io>
2023-06-25 21:12:58 +0000

    build based on 65165fd

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Wordlings","category":"page"},{"location":"#Wordlings","page":"Home","title":"Wordlings","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Wordlings.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Wordlings]","category":"page"}]
+}
